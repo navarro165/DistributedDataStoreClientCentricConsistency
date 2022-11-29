8ac81297a4412e8ee9d1788af14346f14bed5531
Carlos Navarro <navarro165@gmail.com>
2022-11-28 22:16:46 -0600

    add tests objects

--- test_input_output.py
+++ test_input_output.py
@@ -1,101 +1,30 @@
-input_test = [
+input_test_1 = [
     {
         "id": 1,
         "type": "customer",
         "events": [
-            {
-                "id": 1,
-                "interface": "query",
-                "money": 400
-            }
-        ]
+            {"interface": "deposit", "money": 400, "id": 1, "dest": 1},
+            {"interface": "withdraw", "money": 400, "id": 2, "dest": 2},
+            {"interface": "query", "id": 3, "dest": 2},
+        ],
     },
-    {
-        "id": 2,
-        "type": "customer",
-        "events": [
-            {
-                "id": 2,
-                "interface": "deposit",
-                "money": 170
-            },
-            {
-                "id": 3,
-                "interface": "query",
-                "money": 400
-            }
-        ]
-    },
-    {
-        "id": 3,
-        "type": "customer",
-        "events": [
-            {
-                "id": 4,
-                "interface": "withdraw",
-                "money": 70
-            },
-            {
-                "id": 5,
-                "interface": "query",
-                "money": 400
-            }
-        ]
-    },
-    {
-        "id": 1,
-        "type": "branch",
-        "balance": 400
-    },
-    {
-        "id": 2,
-        "type": "branch",
-        "balance": 400
-    },
-    {
-        "id": 3,
-        "type": "branch",
-        "balance": 400
-    }
+    {"id": 1, "type": "branch", "balance": 0},
+    {"id": 2, "type": "branch", "balance": 0},
 ]
 
-expected_output = [
+expected_output_1 = [{"id": 1, "balance": 0}]
+
+input_test_2 = [
     {
         "id": 1,
-        "recv": [
-            {
-                "interface": "query",
-                "result": "success",
-                "money": 500
-            }
-        ]
+        "type": "customer",
+        "events": [
+            {"interface": "deposit", "money": 400, "id": 1, "dest": 1},
+            {"interface": "query", "id": 2, "dest": 2},
+        ],
     },
-    {
-        "id": 2,
-        "recv": [
-            {
-                "interface": "deposit",
-                "result": "success"
-            },
-            {
-                "interface": "query",
-                "result": "success",
-                "money": 500
-            }
-        ]
-    },
-    {
-        "id": 3,
-        "recv": [
-            {
-                "interface": "withdraw",
-                "result": "success"
-            },
-            {
-                "interface": "query",
-                "result": "success",
-                "money": 500
-            }
-        ]
-    }
+    {"id": 1, "type": "branch", "balance": 0},
+    {"id": 2, "type": "branch", "balance": 0},
 ]
+
+expected_output_2 = [{"id": 1, "balance": 400}]
