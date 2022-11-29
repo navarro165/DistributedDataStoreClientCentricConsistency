61a92bc4890e6f0f88d2ac7047b9320b46e96e22
Carlos Navarro <navarro165@gmail.com>
2022-11-29 11:00:09 -0600

    refactor + add write set validation at the customer level

--- customer.py
+++ customer.py
@@ -1,10 +1,15 @@
 import logging
 import grpc
+import json
+
 import banking_pb2
 import banking_pb2_grpc
 
 
 class Customer:
+
+    supported_write_operations = {"deposit", "withdraw"}
+
     def __init__(self, _id: int, events: list):
         # unique ID of the Customer
         self.id = _id
@@ -12,35 +17,48 @@ class Customer:
         # events from the input
         self.events = events
 
-        # a list of received messages used for debugging purpose
-        self.recvMsg = list()
-
-        # pointer for the stub
-        self.stub = None
-
-    def create_stub(self) -> None:
-        """Helper to facilitate communication between customers and a branch process with matching ID"""
-        with grpc.insecure_channel(f'localhost:5005{self.id}') as channel:
-            self.stub = banking_pb2_grpc.BranchStub(channel)
-            self.execute_events()
-
     def execute_events(self) -> None:
         """Processes the events from the list of events and submits the request to the Branch process"""
-        logging.debug(f"\n\nExecuting customer {self.id} events...")
+
+        customer_write_sets = []
+
+        # will save the write sets from the last branch in the loop
+        # (which should have all the write sets in the correct order)
+        executed_branch_write_sets = None
 
         for event in self.events:
-            logging.debug(f"\n\n\t###################")
-            logging.debug(f"\t##### {event['interface'].upper()} #####")
-            logging.debug(f"\t###################")
 
-            request = banking_pb2.BranchRequest(
-                interface=event['interface'],
-                money=event.get('money'),
-                type="customer",
-                id=self.id
-            )
-            response = self.stub.MsgDelivery(request)
+            # facilitate communication between customers and a branch process with matching ID
+            with grpc.insecure_channel(f"localhost:5005{event['dest']}") as channel:
 
-            logging.debug(f"\t^ customer {self.id} confirms that branch {self.id} "
-                          f"has{' ' if event['interface'] == 'query' else ' new '}"
-                          f"balance of {response.balance}".upper())
+                # will be used to verify that the customer's write sets
+                # were executed in the correct order by the branch
+                if event["interface"] in self.supported_write_operations:
+                    customer_write_sets.append(event["id"])
+
+                stub = banking_pb2_grpc.BranchStub(channel)
+                request = banking_pb2.BranchRequest(
+                    event_id=event["id"],
+                    interface=event["interface"],
+                    money=event.get("money"),
+                    type="customer",
+                    id=self.id,
+                    customer_id=self.id,
+                )
+                response = stub.MsgDelivery(request)
+                executed_branch_write_sets = json.loads(response.write_sets)
+
+                if response.status != 200:
+                    logging.error(f"\nFailed to execute customer event: {event}")
+                    raise ValueError(f"Event failed with status: {response.status}")
+
+        logging.info("\nFinished executing customer events:")
+
+        # confirm branch executed write sets match customer write sets
+        if customer_write_sets == executed_branch_write_sets:
+            logging.info("> Successfully executed write events in the correct order")
+
+        else:
+            logging.error(f"> Failed to successfully execute write events in the correct order:\n"
+                          f"> customer_write_sets: {customer_write_sets}\n"
+                          f"> executed_branch_write_sets: {executed_branch_write_sets}")
